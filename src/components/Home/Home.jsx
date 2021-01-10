5a727923d7e9dd7efdf7e08ab8e5e0f076b270b8
codetann <thetannerthomas@gmail.com>
2021-01-10 11:34:53 -0700

    added loading animation

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AppContext } from "../../context/Provider";
 
 export default function Home() {
+  const { loaded } = useContext(AppContext);
   return (
     <div className="Home">
-      <h1>Utah Jazz</h1>
-      <img
-        src="https://ak-static.cms.nba.com/wp-content/uploads/silos/nba/latest/440x700/1628378.png"
-        alt="donovan"
-      />
+      {!loaded && (
+        <div className="loading-container">
+          <div class="lds-ring">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        </div>
+      )}
+      {loaded && (
+        <div className="Home">
+          <h1>Utah Jazz</h1>
+          <img
+            src="https://ak-static.cms.nba.com/wp-content/uploads/silos/nba/latest/440x700/1628378.png"
+            alt="donovan"
+          />
+        </div>
+      )}
     </div>
   );
 }
